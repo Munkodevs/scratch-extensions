aa9a3bd21dbf24ad46297e7d03f4c7c632652bb3
Munko <47465864+Munkodevs@users.noreply.github.com>
2021-07-09 15:02:07 -0300

    Update base64.js

--- base64.js
+++ base64.js
@@ -1,5 +1,3 @@
-const icon = "PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0OCIgaGVpZ2h0PSI0OCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiMwMDAwMDAiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIj48cGF0aCBkPSJNMTQgMkg2YTIgMiAwIDAgMC0yIDJ2MTZjMCAxLjEuOSAyIDIgMmgxMmEyIDIgMCAwIDAgMi0yVjhsLTYtNnoiLz48cGF0aCBkPSJNMTQgM3Y1aDVNMTYgMTNIOE0xNiAxN0g4TTEwIDlIOCIvPjwvc3ZnPg==";
-
 class Base64 {
 
   constructor() {}
@@ -13,8 +11,6 @@ class Base64 {
       color2: '#b34242',
       color3: '#9f3838',
 
-      menuIconURI: icon,
-
       blocks: [
         {
           opcode: 'decodeBase64',
@@ -23,7 +19,7 @@ class Base64 {
 
           text: 'decode [DECODEVAL] from base64',
           arguments: {
-            URL: {
+            DECODEVAL: {
               type: Scratch.ArgumentType.STRING,
               defaultValue: 'SGkh'
             }
@@ -36,7 +32,7 @@ class Base64 {
 
           text: 'encode [ENCODEVAL] from base64',
           arguments: {
-            URL: {
+            ENCODEVAL: {
               type: Scratch.ArgumentType.STRING,
               defaultValue: 'Hi!'
             }
